8fa6e60f0826fd0e9d2d90845b5a6e580e2563e0
Curtis Mitchell <70792951+curtisjm@users.noreply.github.com>
2025-09-07 15:22:48 -0700

    init 2

---
+++ scripts/setup.sh
@@ -0,0 +1,4 @@
+xcode-select --install
+curl -fsSL https://install.determinate.systems/nix | sh -s -- install
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
